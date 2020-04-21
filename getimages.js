9d150e3e3e4c302b81df69f201a3fcca8cdb9aa3
JonLebron <40630389+QuebecYankee@users.noreply.github.com>
2020-04-20 18:27:56 -0700

    finished

--- getimages.js
+++ getimages.js
@@ -1,19 +1,16 @@
-document.write("
-<table>
-		<tr>
-			<td class="leftImage"><img alt="Lebron's Veins" src="veins.png"></td>
-			<td><img alt="Lebron's Veins" src="veins.png"></td>
-		</tr>
-		<tr>
-			<td class="leftImage"><img alt="Lebron's Veins" src="veins.png"></td>
-			<td><img alt="Lebron's Veins" src="veins.png"></td>
-		</tr>
-		<tr>
-			<td class="leftImage"><img alt="Lebron's Veins" src="veins.png"></td>
-			<td><img alt="Lebron's Veins" src="veins.png"></td>
-		</tr>
-		<tr>
-			<td class="leftImage"><img alt="Lebron's Veins" src="veins.png"></td>
-		</tr>
-</table>
-");
+var lebronNames = ["Lebron's Veins", "Lebron in Flames"];
+var lebronLinks = ["https://i.ibb.co/wKjmzWY/lebron-s-veins.png", "https://i.ibb.co/2KyCKHm/lebron-in-flames.png"];
+
+var rows = Math.floor(lebronNames.length / 2);
+var nextLebron = 0;
+
+function addRow() {
+	var table = document.getElementById("table");
+	var row = table.insertRow(0);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	cell1.innerHTML = "<img src=" + lebronLinks[nextLebron] + " class=\"leftImage\" alt=\"" + lebronNames[nextLebron] + "\">";
+	nextLebron++;
+	cell2.innerHTML = "<img src=" + lebronLinks[nextLebron] + " class=\"rightImage\" alt=\"" + lebronNames[nextLebron] + "\">";
+	nextLebron++;
+}
